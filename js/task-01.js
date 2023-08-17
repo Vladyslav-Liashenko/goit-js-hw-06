3406f551a2feb61785b2d404949f4255eb04dff3
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-08-17 21:25:32 +0300

    1hw modul 1

---
+++ js/task-01.js
@@ -0,0 +1,38 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+
+const liItem = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${liItem.length}`);
+
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента(тегу < h2 >) і кількість елементів в категорії(усіх < li >, вкладених в нього).
+
+liItem.forEach(liItem => {
+    const childLiItem = liItem.querySelector('h2');
+    const elementsChildLiItem = 0;
+    console.log(childLiItem.textContent);
+    console.log(elementsChildLiItem);
+});
+
+
+
+// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+
+
+
+
