c78a973e2f3d79e0debfeb76131edaa22950d4c5
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-08-23 15:12:52 +0300

    hw 8 fimnal

--- js/task-08.js
+++ js/task-08.js
@@ -19,3 +19,20 @@
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    if (!email || !password) {
+        alert("Please fill in all the fields");
+        return;
+    }
+
+    console.log(`Login: ${email}, Password: ${password}`);
+    form.reset();
+};
